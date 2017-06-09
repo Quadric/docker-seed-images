dbe4aa0ddef6bda7ab6c6334b4a97121ba723b29
Ahmad Magdy <magdy.a@hotmail.com>
2017-06-09 15:03:50 +0200

    Update mongo-connector run.sh scripts with 2 echo statements

--- mongo-connector_2.3/run.sh
+++ mongo-connector_2.3/run.sh
@@ -16,7 +16,9 @@ if [ ! -f "$MONGO_CONNECTOR_CONFIG_FILE_PATH" ] ; then
     exit 1;
   fi
 
+  echo "Creating $MONGO_CONNECTOR_CONFIG_FILE_PATH from env var MONGO_CONNECTOR_CONFIG .."
   echo "$MONGO_CONNECTOR_CONFIG" | sed -e 's/\\\\040/ /g' | python3 -m json.tool > "$MONGO_CONNECTOR_CONFIG_FILE_PATH"
 fi
 
+echo "Start the app";
 mongo-connector -c "$MONGO_CONNECTOR_CONFIG_FILE_PATH"
