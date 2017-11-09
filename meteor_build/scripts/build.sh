c15c25bf388d961fad8d6333293c45f1d53881a0
Ahmad Magdy <magdy.a@hotmail.com>
2017-11-09 10:29:19 +0100

    Add initial meteor_build image

---
+++ meteor_build/scripts/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -eox pipefail;
+
+# COPIED_APP_PATH=/copied-app
+CONTAINER_BUNDLE_DIR=${CONTAINER_BUNDLE_DIR:-/tmp/bundle-dir}
+
+# # sometimes, directly copied folder cause some wierd issues
+# # this fixes that
+# echo "=> Copying the app"
+# cp -R /app $COPIED_APP_PATH
+# cd $COPIED_APP_PATH
+
+# echo "=> Executing NPM install --production"
+# meteor npm install --production --quiet
+
+echo "=> Executing Meteor Build..."
+# export
+meteor build \
+. \
+--allow-superuser \
+--directory $BUNDLE_DIR \
+--architecture os.linux.x86_64
+# --server=http://localhost:3000 # TODO send the server if we are building for mobile
+
+echo "=> Printing Meteor Node information..."
+echo "  => platform"
+meteor node -p process.platform
+echo "  => arch"
+meteor node -p process.arch
+echo "  => versions"
+meteor node -p process.versions
+
+echo "=> Printing System Node information..."
+echo "  => platform"
+node -p process.platform
+echo "  => arch"
+node -p process.arch
+echo "  => versions"
+node -p process.versions
+
+# echo "=> Executing NPM install within Bundle"
+# cd $BUNDLE_DIR/bundle/programs/server/
+# npm i
+# 
+# echo "=> Moving bundle"
+# mv $BUNDLE_DIR/bundle /built_app
+# 
+# echo "=> Cleaning up"
+# # cleanup
+# echo " => COPIED_APP_PATH"
+# rm -rf $COPIED_APP_PATH
+# echo " => BUNDLE_DIR"
+# rm -rf $BUNDLE_DIR
+# echo " => .meteor"
+# rm -rf ~/.meteor
+# rm /usr/local/bin/meteor
